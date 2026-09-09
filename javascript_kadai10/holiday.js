95be2456e5758cde9bb684dc0ac5c97dadeadb12
KAWAGUCHI-70 <n.river.woow@gmail.com>
2026-09-10 00:41:15 +0900

    Update holiday.js

--- javascript_kadai10/holiday.js
+++ javascript_kadai10/holiday.js
@@ -3,14 +3,14 @@ const holidays = ["正月","成人の日","建国記念日","天皇誕生日","�
 // for文の場合
 console.log('for文');
 for (let cnt_x = 0; cnt_x <16; cnt_x++) {
-  console.log(cnt_x + holidays[cnt_x]);
+  console.log( holidays[cnt_x]);
 }
 
 // while文の場合
 let cnt_y = 0;
  console.log('while文')
 while (cnt_y <16) {
-  console.log(cnt_y + holidays[cnt_y]);
+  console.log(holidays[cnt_y]);
   cnt_y++;
 
 }
