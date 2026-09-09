97b9a4caf842b24b99301fe190f12d0f0ed48f47
KAWAGUCHI-70 <n.river.woow@gmail.com>
2026-09-10 02:37:42 +0900

    Add files via upload

--- javascript_kadai007/calc.js
+++ javascript_kadai007/calc.js
@@ -1,14 +1,11 @@
-let num = 15;
-
-//3の倍数の時
-if(num % 3  == 0) { 
+let num = Math.floor(Math.random() * 31);
+ 
+if(num % 3 == 0 && num % 5 == 0){
+ console.log('3と5の倍数です');
+}else if(num % 3 == 0){
   console.log('3の倍数です');
-}
-//5の倍数の時
-if(num % 5  == 0) { 
-  console.log('５の倍数です');
-}
-
-if(num % 5  == 0 && num % 3 == 0) { 
-  console.log('３と５の倍数です');
+}else if(num % 5 == 0){
+  console.log('5の倍数です');
+}else{
+  console.log(num);
 }
